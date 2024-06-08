fba8f2dff9af5d69d281116433d972388fd4121b
amaral220x <biel.ilha2021@gmail.com>
2024-06-08 12:10:01 -0300

    feat: Rotas de get feitas

--- src/main/java/com/placeholder/bobanco/controller/ClienteController.java
+++ src/main/java/com/placeholder/bobanco/controller/ClienteController.java
@@ -1,15 +1,20 @@
 package com.placeholder.bobanco.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.placeholder.bobanco.repository.ClienteRepository;
 import com.placeholder.bobanco.model.entity.Cliente;
+import com.placeholder.bobanco.model.value.Cpf;
+import com.placeholder.bobanco.model.value.Email;
 
 import java.util.List;
-
-
+import java.util.Optional; 
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/cliente")
@@ -24,5 +29,24 @@ public class ClienteController {
     public List<Cliente> all() {
         return repository.findAll();
     }
-    
+    @GetMapping("/cpf/{cpf}")
+    public Cliente one(@PathVariable String cpf) {
+        Cpf cpfObj = new Cpf(cpf); 
+        Optional<Cliente> cliente = repository.findByCpf(cpfObj); // Store the Optional object
+        return cliente.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+    }
+    @GetMapping("/email/{email}")
+    public Cliente oneByEmail(@PathVariable String email) {
+        Email emailObj = new Email(email);
+        Optional<Cliente> cliente = repository.findByEmail(emailObj); // Store the Optional object
+        return cliente.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+    }
+    @GetMapping("/id/{id}")
+    public Cliente oneById(@PathVariable UUID id) {
+        Optional<Cliente> cliente = repository.findById(id); // Store the Optional object
+        return cliente.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+    }
+
+
+        
 }
--- src/main/java/com/placeholder/bobanco/repository/ClienteRepository.java
+++ src/main/java/com/placeholder/bobanco/repository/ClienteRepository.java
@@ -1,11 +1,18 @@
 package com.placeholder.bobanco.repository;
 
 import com.placeholder.bobanco.model.entity.Cliente;
+import com.placeholder.bobanco.model.value.Cpf;
+import com.placeholder.bobanco.model.value.Email;
+
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<Cliente, UUID>{
+    Optional<Cliente> findByCpf(Cpf cpf);
 
     
+    Optional<Cliente> findByEmail(Email email);
+
 }
